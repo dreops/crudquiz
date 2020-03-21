c34ab37410fc354ad7f3c0aa5d14b14d0c4bdf89
dreops <dreops@andreasandrews.com>
2020-03-21 01:00:30 +0000

    added basic html content for all current pages

--- application/routes.py
+++ application/routes.py
@@ -5,3 +5,15 @@ from application import app
 @app.route('/home')
 def home():
  return render_template('home.html', title='Home')
+
+@app.route('/add')
+def add():
+    return render_template('add.html', title='Add Questions')
+
+@app.route('/answers')
+def add():
+    return render_template('answers.html', title='Answers')
+
+@app.route('/quiz')
+def add():
+    return render_template('quiz.html', title='The Quiz')
