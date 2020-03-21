235f21157b44d3050a73d6d84e80815db2b1d19e
dreops <dreops@andreasandrews.com>
2020-03-21 01:34:14 +0000

    Error building url endpoint!

--- application/routes.py
+++ application/routes.py
@@ -4,16 +4,16 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home')
 
 @app.route('/add')
 def add():
     return render_template('add.html', title='Add Questions')
 
 @app.route('/answers')
-def add():
+def answers():
     return render_template('answers.html', title='Answers')
 
 @app.route('/quiz')
-def add():
+def quiz():
     return render_template('quiz.html', title='The Quiz')
