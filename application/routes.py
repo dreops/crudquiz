c5d4b439ef5d978d8f28c25e36fef258bdf0ebbb
dreops <dreops@andreasandrews.com>
2020-03-21 02:34:20 +0000

    html files and layout tidied up before bed so I can sleep better zzz

--- application/routes.py
+++ application/routes.py
@@ -6,6 +6,10 @@ from application import app
 def home():
     return render_template('home.html', title='Home')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
 @app.route('/add')
 def add():
     return render_template('add.html', title='Add Questions')
