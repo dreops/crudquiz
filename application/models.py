4686c6aae3a039d727e523b72682890674b66ada
dreops <dreops@andreasandrews.com>
2020-03-23 20:41:57 +0000

    updated models.py

--- application/models.py
+++ application/models.py
@@ -3,10 +3,12 @@ from application import db
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quizquestions = db.Column(db.String(255), unique=True)
-    answer_id = db.Column(db.Integer)
+    answer_id = db.relationship('Answers', backref='', lazy=True)
 
     def __repr_(self):
         return f"Questions('{self.quizquestions}')"
 
-# h = Questions()
-# print(h)
+class Answers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    users_answers = db.Column(db.Integer(10), unique=True)
+    correct_answer = db.Column(db.Integer(10), nullable=False)
