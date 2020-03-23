75430dd830779b43be1c88836399b243b252314e
dreops <dreops@andreasandrews.com>
2020-03-23 21:02:36 +0000

    added Create link to navbar on layout.html

--- application/models.py
+++ application/models.py
@@ -12,3 +12,6 @@ class Answers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     users_answers = db.Column(db.Integer(10), unique=True)
     correct_answer = db.Column(db.Integer(10), nullable=False)
+
+    def __repr_(self):
+        return f"Answers('{self.users_answers}')"
