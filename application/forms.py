dbbd2fb2a9c8309120a1fd7ce25b054ec70169a3
dreops <dreops@andreasandrews.com>
2020-03-23 18:41:01 +0000

    fixed typo

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, IntergerField
+from wtforms import StringField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
 
